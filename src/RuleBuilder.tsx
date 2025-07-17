1dc10c620192d850d3e3e856650cbe20723ecf51
smartconfigui <alpcanutku@gmail.com>
2025-07-17 13:48:55 -0700

    Updated RuleBuilder.tsx

--- src/RuleBuilder.tsx
+++ src/RuleBuilder.tsx
@@ -499,6 +499,11 @@ export default function RuleBuilder() {
       if (condition.useRepeat) {
         const operator = condition.repeat ? Object.keys(condition.repeat)[0] : '==';
         condition.repeat = { [operator]: Number(value) };
+      } else {
+        const operator = condition.count ? Object.keys(condition.count)[0] : '==';
+        condition.count = { [operator]: Number(value) };
+      }
+    } else if (field === 'repeat') {
       const operator = condition.repeat ? Object.keys(condition.repeat)[0] : '>=';
       condition.repeat = { [operator]: Number(value) };
     } else if (field === 'countOperator') {
